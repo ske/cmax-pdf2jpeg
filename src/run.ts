f567734e3087a5fdf0d44ae73451ceccfdef0ff5
Balazs Skerlecz <skerlecz.balazs@gmail.com>
2020-11-28 22:04:39 +0100

    Adding process signal handlers

--- src/run.ts
+++ src/run.ts
@@ -3,21 +3,29 @@ import {Pdf2JpegWorker} from "./Pdf2JpegWorker";
 import {Server} from "ts-jobserver-simple/dist/core/Server";
 
 const logger = new Logger({name: "pdf2jpeg-job-server"});
+
 try {
     logger.info("starting");
+
     const jobServer = new Server({
         logger: logger,
         workdir: '/tmp',
         port: 8080
     });
-    process.on('SIGINT', async () => {
-        logger.info("SIGINT received, shutting down.");
-        await jobServer.shutdown();
-    });
+
+    const sighandler = async () => {
+	logger.info("Stop signal received, shutting down.");
+	await jobServer.shutdown();
+	process.exit(0);
+    };
+
+    process.on('SIGINT', sighandler);
+    process.on('SIGHUP', sighandler);
+    process.on('SIGTERM', sighandler);
 
     jobServer.registerWorkerType(Pdf2JpegWorker);
 
-    Promise.all([jobServer.start()]).then(() => {
+    jobServer.start().then(() => {
         logger.info("started");
     }).catch((e) => {
         logger.error("cannot start", e);
